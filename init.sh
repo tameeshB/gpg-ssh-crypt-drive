dcf17b8b9e1b71938f7573e213a2b13ebc7f1661
tameeshb <me@tameesh.in>
2017-10-30 03:56:01 +0530

    Init script

---
+++ init.sh
@@ -0,0 +1,8 @@
+mkdir oldroot
+mkdir data
+echo "Now store all data inside the data/ directory and then run 'sout' to encrypt and exit "
+touch ~/.bashrc
+echo "#ssh-crypt-drive" >> ~/.bashrc
+echo "alias sout='./logout.sh && exit'" >> ~/.bashrc
+echo "alias lin='./login.sh'" >> ~/.bashrc
+source ~/.bashrc
